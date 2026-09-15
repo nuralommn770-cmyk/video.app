5d09346f3822aaa7f7a16f644d5698f17fb96644
nuralommn770-cmyk <nuralommn770@gmail.com>
2026-09-15 17:00:58 +0600

    Update Project-level build.gradle.kts

--- Project-level build.gradle.kts
+++ Project-level build.gradle.kts
@@ -1,1 +1,6 @@
-hello
+// Top-level build file
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
